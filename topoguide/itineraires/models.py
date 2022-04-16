fa7d612d73f39f267cb1dde05ccf633f44ade238
Victor Colombet <victorcolombet@gmail.com>
2022-04-16 22:10:59 +0200

    Modèle de sortie + correction du modèle itinéraire

--- topoguide/itineraires/models.py
+++ topoguide/itineraires/models.py
@@ -9,7 +9,15 @@ class Itineraire(models.Model):
     altitude_max = models.IntegerField(default = 0)
     height_dif_pos = models.IntegerField(default=0)
     height_dif_neg = models.IntegerField(default=0)
-    duration = models.IntegerField(default = 0)
-    difficulty = models.IntegerField(default=1)
+    duration = models.DurationField()
+    difficulty = models.IntegerField(choices = [1,2,3,4,5])
 
-    
+class Sortie(models.Model):
+    user = models.ForeignKey('user', on_delete=models.CASCADE)
+    itineraire = models.ForeignKey(Itineraire, on_delete=models.CASCADE)
+    date = models.DateField()
+    duration = models.DurationField()
+    people = models.IntegerField(default = 1)
+    group_exp = models.CharField(choices= ['tous débutants', 'tous expérimentés', 'mixte'] )
+    weather = models.CharField(choices = ['bonne', 'moyenne', 'mauvaise'])
+    difficulty = models.IntegerField(choices = [1,2,3,4,5])

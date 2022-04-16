2ddeba68018231e04bf863e184902a4786ef1706
Victor Colombet <victorcolombet@gmail.com>
2022-04-16 19:23:28 +0200

    Création du modèle Itineraire

--- topoguide/itineraires/models.py
+++ topoguide/itineraires/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Itineraire(models.Model):
+    title = models.CharField(max_length=200)
+    departure = models.CharField(max_length=200)
+    description = models.TextField(max_length=500)
+    altitude_dep = models.IntegerField(default=0)
+    altitude_min = models.IntegerField(default = 0)
+    altitude_max = models.IntegerField(default = 0)
+    height_dif_pos = models.IntegerField(default=0)
+    height_dif_neg = models.IntegerField(default=0)
+    duration = models.IntegerField(default = 0)
+    difficulty = models.IntegerField(default=1)
+
+    

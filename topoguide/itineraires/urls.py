6818ccf6fe3c00366f13dca7c10796a3ca567d6e
Victor Colombet <victorcolombet@gmail.com>
2022-04-17 08:16:50 +0200

    Création du chemin d'url pour nouvelle sortie correction

--- topoguide/itineraires/urls.py
+++ topoguide/itineraires/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('itineraires/', views.itineraires, name = 'itineraires'),
     path('sorties/<str:itineraire_id>/', views.sorties, name='sorties'),
     path('sortie/<str:sortie_id>/', views.sortie, name='sortie'),
-    path('nouvelle_sortie/', views.views.nouvelle_sortie, name='nouvelle_sortie')
+    path('nouvelle_sortie/', views.nouvelle_sortie, name='nouvelle_sortie')
 ]
